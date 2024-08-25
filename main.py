66df92d06f2b4d7535affc601529a99f71e3c0cd
GaelanKRD-Z <gaelankrd@outlook.com>
2024-08-25 17:32:37 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,12 +1,11 @@
 from run import Bot
-from utils import asyncio
+import asyncio
 from keep_alive import keep_alive
-keep_alive()
 
+keep_alive()
 
 async def main():
     await Bot.initialize()
     await Bot.run()
-
 
 asyncio.run(main())
