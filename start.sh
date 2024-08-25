cf3adf8f4600c2cca14ae588609c6611f1c50377
GaelanKRD-Z <gaelankrd@outlook.com>
2024-08-25 17:43:09 +0300

    Create start.sh

---
+++ start.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+# Activate the virtual environment if you're using one
+# source venv/bin/activate
+
+# Run the Python script
+python3 main.py
+
+# OR if python3 is not available, use:
+# python main.py
